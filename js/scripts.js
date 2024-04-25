367edb13b2faa425f2ba4d3c2b57a04b30b65d18
Emcahell <emmanuelcastroh2002@gmail.com>
2024-04-25 13:43:54 -0430

    logica js para el buscador

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+const search = document.getElementById("search");
+// const cards = document.getElementById("cards");
+
+ search.addEventListener("keyup", e =>{
+
+    // const searchText = e.target.value.toLowerCase();
+    // cards.forEach(card => {
+    //     const countryName = card.querySelectorAll(".country-name").textContent.toLowerCase();
+
+    //     if (countryName.includes(searchText)) {
+    //         card.classList.remove("filter");
+    //     } else {
+    //         card.classList.add("filter");
+    //     }
+    // })
+
+    if(e.target == search){
+        document.querySelectorAll(".country").forEach(el => el.textContent.toLowerCase().includes(e.target.value)
+        ? el.classList.remove("filter")
+        : el.classList.add("filter") 
+    )
+    }
+
+ })
